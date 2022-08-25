606e5f04454ee8a0ed7f21fcf45ef1255eabc44d
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-08-25 12:51:28 +0300

    commit tree 4c91323a8fa1662234fc593e377cb952ebd9b695

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+
+
+require 'pry'
+
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        ref = @word.chars.sort
+        empty = []
+        arr.each do |elem|    
+            if elem.chars.sort == ref
+                empty << elem
+            else
+                next
+            end
+        end
+        return empty
+    end
+end
+
+listen = Anagram.new('listen')
+p listen.match(%w[enlists google inlets banana])
